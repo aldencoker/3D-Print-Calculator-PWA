c41b16c6a71e58a66b6c5a04b2b8fb92ddae35c4
aldencoker <gcoker@ualr.edu>
2026-03-13 02:10:17 +0000

    insipration code added

---
+++ print_price_calculator.py
@@ -0,0 +1,12 @@
+des_time_input = input("Design Time: ")
+mat = float(input("Mass: "))
+prnt_time = float(input("Print Time: "))
+if des_time_input == "":
+    des_time = 0
+else:
+    des_time = float(des_time_input)
+cost = ((des_time * 30) + (mat * 0.05) + prnt_time)
+rounded_cost = round(cost / 5) * 5
+if rounded_cost == 0:
+    rounded_cost = 5
+print(f"${rounded_cost:.0f}")
